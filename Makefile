aa4a92e03c24c2e831929aa2b345f79d5975b270
Nicholas Gallimore <nfg3@zips.uakron.edu>
2017-02-14 20:31:29 -0500

    min

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ install:
 	g++ Rectangle.o rectangle_main.cpp -o rectangle_main -std=c++11
 clean:
 	rm -rf *.o rectangle_main
+
+run:
+	./rectangle_main
--- Rectangle.hpp
+++ Rectangle.hpp
@@ -1,23 +1,4 @@
 #ifndef Rectangle_hpp
 #define Rectangle_hpp
-
-struct Point {
-	double x,y;
-};
-
-class Rectangle 
-{
-private:
-	Point a1, a2, a3, a4;
-	bool isValidRectangle(Point p1, Point p2, Point p3, Point p4);
-
-public:
-	double length();
-	double width();
-	Rectangle(Point p1, Point p2, Point p3, Point p4);
-	double perimeter();
-	double area();
-	void setCoord(Point p1, Point p2, Point p3, Point p4);
-	double square();
-};
+struct Point{double x,y;};class Rectangle{private:Point a1,a2,a3,a4;bool b(Point p1,Point p2,Point p3,Point p4);public:double length(),width(),perimeter(),square(),area();Rectangle(Point p1,Point p2,Point p3,Point p4);void setCoord(Point p1,Point p2,Point p3,Point p4);};
 #endif
